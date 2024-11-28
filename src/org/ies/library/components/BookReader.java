50825d301cfe71ec82764c2e6251c63f064e4b1c
Andsan03 <andrewabellosanchez123456@gmail.com>
2024-11-28 11:07:42 +0100

    Author y book reader

--- src/org/ies/library/components/BookReader.java
+++ src/org/ies/library/components/BookReader.java
@@ -1,4 +1,41 @@
 package org.ies.library.components;
 
+import org.ies.library.model.Autor;
+import org.ies.library.model.Book;
+
+import java.util.Scanner;
+
 public class BookReader {
+    private final Scanner scanner;
+    private final AuthorReader authorReader;
+
+    public BookReader (Scanner scanner, AuthorReader authorReader) {
+        this.scanner = scanner;
+        this.authorReader = authorReader;
+    }
+
+    public Book read() {
+        System.out.println("Introduce los datos del libro");
+        System.out.println("ISBN");
+        String isbn = scanner.nextLine();
+        System.out.println("Titulo");
+        String title = scanner.nextLine();
+        System.out.println("Año");
+        int year = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Cuantos autores tiene");
+        int size = scanner.nextInt();
+        scanner.nextLine();
+
+        Autor[] authors = new Autor[size];
+        for (int i = 0; i < authors.length; i++) {
+            authors[i] = authorReader.read();
+        }
+        return new Book(
+                isbn,
+                title,
+                year,
+                authors
+        );
+    }
 }
